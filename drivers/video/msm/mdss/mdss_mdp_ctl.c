516ff56f5799f78f41dcd0aad626fd4745836f24
fergy <ramon.rebersak@gmail.com>
2015-01-30 14:57:15 +0100

    MDP: typo correct

--- drivers/video/msm/mdss/mdss_mdp_ctl.c
+++ drivers/video/msm/mdss/mdss_mdp_ctl.c
@@ -1758,8 +1758,6 @@ int mdss_mdp_ctl_setup(struct mdss_mdp_ctl *ctl)
 		ctl->mixer_right->is_right_mixer = true;
 		ctl->mixer_right->width = width;
 		ctl->mixer_right->height = height;
-		//ctl->mixer_right->roi = (struct mdss_rect)
-						{0, 0, width, height};
 		ctl->mixer_right->roi = (struct mdss_mdp_img_rect)
 						{0, 0, width, height};
 	} else if (ctl->mixer_right) {
